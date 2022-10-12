334605b6796fb9818120a53ad8d651cf3c03a0b8
João Rhoden <86614864+joaorhodenntc@users.noreply.github.com>
2022-10-12 17:01:56 -0300

    End

--- ConverteSentenca.java
+++ ConverteSentenca.java
@@ -8,14 +8,19 @@ public class ConverteSentenca {
         } else {
             for (int i = 0; i < sentenca.length(); i++) {
                 char[] sentencaArray = sentenca.toCharArray();
+
                 if (Character.isLowerCase(sentencaArray[i])) {
                     naoTerminal.adicionaNaoTerminal(sentencaArray[i]);
-                } else {
+                } else if (Character.isUpperCase(sentencaArray[i])) {
                     terminal.adicionaTerminal(sentencaArray[i]);
                 }
             }
             return true;
         }
+    }
+
+    public Character imprimeInicial() {
+        return terminal.getTerminais(0);
     }
 
     public String imprimeTerminal() {
--- Main.java
+++ Main.java
@@ -7,14 +7,14 @@ public class Main {
         ConverteSentenca converte = new ConverteSentenca();
 
         System.out.println("Digite a sentença: ");
-        String inputSentenca = sc.nextLine();
-        converte.converteSentenca(inputSentenca);
+        String inputSentenca = null;
+        while (inputSentenca != "") {
+            inputSentenca = sc.nextLine();
+            converte.converteSentenca(inputSentenca);
+        }
         System.out.print("G = {" + converte.imprimeTerminal() + "},");
-        System.out.println(" { " + converte.imprimeNaoTerminal() + "}");
-
-        // System.out.println("Digite os nao terminais: ");
-        // String inputNaoTerminal = sc.nextLine();
-        // naoTerminal.adicionaNaoTerminal(inputNaoTerminal);
+        System.out.println(" {" + converte.imprimeNaoTerminal() + "},P," +
+                converte.imprimeInicial());
 
         sc.close();
 
